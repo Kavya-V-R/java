50d3f958e08a72d8da9f10b63675d9e0c65b140d
Kavya-V-R <89826872+Kavya-V-R@users.noreply.github.com>
2021-09-04 09:51:25 +0530

    Add files via upload

---
+++ Explicit.java
@@ -0,0 +1,9 @@
+class Explicit
+{
+public static void main(String args[])
+{
+int i=50;
+float f=i;
+System.out.println("Int value="+i);
+System.out.println("Float value="+f);
+}}
---
+++ Implicit.java
@@ -0,0 +1,9 @@
+class Implicit
+{
+public static void main(String args[])
+{
+double d=82.204;
+int i=(int)d;
+System.out.println("double value="+d);
+System.out.println("int value="+i);
+}}
